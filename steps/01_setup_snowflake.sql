4af225825b559ec1b24bf828653062f4122f9eb3
Dominick Rocco <rocconnick@users.noreply.github.com>
2023-06-01 03:35:25 +0000

    Remove the things that the sandbox admin will do.

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -16,33 +16,10 @@ Last Updated: 1/1/2023
 -- ----------------------------------------------------------------------------
 -- Step #2: Create the account level objects
 -- ----------------------------------------------------------------------------
-USE ROLE ACCOUNTADMIN;
-
--- Roles
-SET MY_USER = CURRENT_USER();
-CREATE OR REPLACE ROLE HOL_ROLE;
-GRANT ROLE HOL_ROLE TO ROLE SYSADMIN;
-GRANT ROLE HOL_ROLE TO USER IDENTIFIER($MY_USER);
-
-GRANT EXECUTE TASK ON ACCOUNT TO ROLE HOL_ROLE;
-GRANT MONITOR EXECUTION ON ACCOUNT TO ROLE HOL_ROLE;
-GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE TO ROLE HOL_ROLE;
-
--- Databases
-CREATE OR REPLACE DATABASE HOL_DB;
-GRANT OWNERSHIP ON DATABASE HOL_DB TO ROLE HOL_ROLE;
-
--- Warehouses
-CREATE OR REPLACE WAREHOUSE HOL_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPEND = 300, AUTO_RESUME= TRUE;
-GRANT OWNERSHIP ON WAREHOUSE HOL_WH TO ROLE HOL_ROLE;
-
 
 -- ----------------------------------------------------------------------------
 -- Step #3: Create the database level objects
 -- ----------------------------------------------------------------------------
-USE ROLE HOL_ROLE;
-USE WAREHOUSE HOL_WH;
-USE DATABASE HOL_DB;
 
 -- Schemas
 CREATE OR REPLACE SCHEMA EXTERNAL;
